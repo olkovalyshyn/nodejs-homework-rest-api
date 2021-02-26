dee06339f96ac534ff145461d22a9700cc7ab265
Olexandr Kovalyshyn <kovalyshyn@gmail.com>
2021-02-26 19:25:15 +0200

    Deleted comments

--- model/index.js
+++ model/index.js
@@ -1,83 +1,25 @@
 const Contact = require('../model/shemas/shema-contact');
 
-// const fs = require("fs/promises");
-// const fs = require('fs').promises;
-// const path = require('path');
-// const shortid = require("shortid");
-
-// const contactsPath = path.join(__dirname, './contacts.json');
-
-// const listContacts = async () => {
-//   const data = await fs.readFile(contactsPath, 'utf8');
-//   return JSON.parse(data);
-// };
 const listContacts = async () => {
   const data = await Contact.find({});
   return data;
 };
 
-// const getContactById = async contactId => {
-//   const contacts = await listContacts();
-//   const contact = contacts.find(({ id }) => id.toString() === contactId);
-//   return contact;
-// };
 const getContactById = async contactId => {
   const contact = await Contact.findOne({ _id: contactId });
   return contact;
 };
 
-// const removeContact = async contactId => {
-//   const contacts = await listContacts();
-//   const contact = contacts.find(({ id }) => id.toString() === contactId);
-//   const newContacts = contacts.filter(({ id }) => id.toString() !== contactId);
-//   await fs.writeFile(
-//     contactsPath,
-//     JSON.stringify(newContacts, null, 2),
-//     'utf8',
-//   );
-//   return contact;
-// };
 const removeContact = async contactId => {
   const contact = await Contact.findByIdAndRemove({ _id: contactId });
   return contact;
 };
 
-// const addContact = async body => {
-//   const contacts = await listContacts();
-//   const newContact = {
-//     // id: shortid.generate(),
-//     ...body,
-//   };
-//   const newContacts = [...contacts, newContact];
-//   await fs.writeFile(
-//     contactsPath,
-//     JSON.stringify(newContacts, null, 2),
-//     'utf8',
-//   );
-//   return newContact;
-// };
 const addContact = async body => {
   const newContact = await Contact.create(body);
   return newContact;
 };
 
-// const updateContact = async (contactId, body) => {
-//   try {
-//     const contacts = await listContacts();
-//     const indexContactForUpdate = contacts.findIndex(
-//       ({ id }) => id.toString() === contactId,
-//     );
-//     if (indexContactForUpdate === -1) return;
-//     contacts[indexContactForUpdate] = {
-//       ...contacts[indexContactForUpdate],
-//       ...body,
-//     };
-//     await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2), 'utf8');
-//     return contacts[indexContactForUpdate];
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
 const updateContact = async (contactId, body) => {
   const contact = await Contact.findByIdAndUpdate(
     { _id: contactId },
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-// const contactsActions = require("../../model/index");
 const validate = require('./validation');
 const contactsControllers = require('../../controllers/contacts-controllers');
 
